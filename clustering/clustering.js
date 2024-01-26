5a2d94ee846fc138f867ab218d7fb76a4116d48a
Venusqh <146376105+Venusqh@users.noreply.github.com>
2024-01-25 19:09:05 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 8,
     center: { lat: 49.491, lng: -83.661 },
   });
   // Create an array of alphabetical characters used to label the markers.
